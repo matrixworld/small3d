733401bd991e8d56da2daaa16995c534672e1cc7
Dimitri Kourkoulis <developer@dimitros.be>
2014-11-28 01:45:03 +0100

    Stable release documentation

--- search/variables_4.js
+++ search/variables_4.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['lightdirection',['lightDirection',['../classsmall3d_1_1_renderer.html#a77353e63ccea1c4f33ff9cba9eb81978',1,'small3d::Renderer']]],
+  ['lightintensity',['lightIntensity',['../classsmall3d_1_1_renderer.html#a16cad6462cacc418e9d470fa90677c40',1,'small3d::Renderer']]],
   ['logger',['logger',['../_logger_8cpp.html#a602215fa5e4da4e67434e454bbbf39ae',1,'logger():&#160;Logger.cpp'],['../_logger_8h.html#a602215fa5e4da4e67434e454bbbf39ae',1,'logger():&#160;Logger.cpp']]]
 ];
